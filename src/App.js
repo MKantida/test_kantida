fbdd30de4f8e7f986e944b2cd3a38a9d1c4c33c6
MKantida <krantida-th@hotmail.co.th>
2023-05-17 23:32:51 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import profilepic from '../img/BG.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={profilepic} alt="BG" className="profilepic" />
+                <ReactTypingEffect text={['I am Kantida', 'I am a Web Developer']} speed={50} eraseDelay={100} className="typingeffect" />
+                <Social />
+
+        
       </header>
     </div>
+    
   );
 }
 
