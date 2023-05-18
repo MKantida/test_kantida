bfb2791bdd2a5a33f452d96abf11c95def693e0e
MKantida <97874677+MKantida@users.noreply.github.com>
2023-05-18 10:42:54 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import profilepic from '../img/BG.png';
+//import profilepic from '../img/BG.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <img src={profilepic} alt="BG" className="profilepic" />
-                <ReactTypingEffect text={['I am Kantida', 'I am a Web Developer']} speed={50} eraseDelay={100} className="typingeffect" />
-                <Social />
-
+      
+      
         
-      </header>
+      
     </div>
     
   );
